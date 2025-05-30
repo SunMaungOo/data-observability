abb910adce669cb667bf4f5ed119cf1a1af1b86f
Sun Maung Oo <2162432+SunMaungOo@users.noreply.github.com>
2025-05-31 01:31:56 +0630

    add a manual lineage api code

--- ingest.py
+++ ingest.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import json
-from model import Application,ApplicationRepository,observations_for_df,OutputDataLineage
+from model import (
+    Application,
+    ApplicationRepository,
+    observations_for_df,
+    OutputDataLineage,
+    ApplicationVersion,
+    User,
+    ApplicationExecution,
+    DataLineageExecution,
+    DataMetrics
+)
 
 def main():
 
@@ -11,6 +21,23 @@ def main():
     app_repo = ApplicationRepository(location=ApplicationRepository.fetch_git_location(),application=app)
 
     print(json.dumps(app_repo))
+    
+
+    user = User(name=User.fetch_git_author())
+
+    print(json.dumps(user))
+
+
+    app_version = ApplicationVersion(version=ApplicationVersion.fetch_git_version(),\
+                                     user=user,\
+                                     application_repo=app_repo)
+    
+    print(json.dumps(app_version))
+
+    app_exec = ApplicationExecution(application_version=app_version,\
+                                    user=user)
+
+    print(json.dumps(app_exec))
 
     app_tech = pd.read_csv(
         "data/AppTech.csv",
@@ -19,9 +46,10 @@ def main():
             "Symbol":"category"
         })
 
-    (app_tech_ds,app_tech_schema,app_tech_metric) = observations_for_df(df_name="data/AppTech.csv",\
+    (app_tech_ds,app_tech_schema) = observations_for_df(df_name="data/AppTech.csv",\
                         df_format="csv",\
                         df=app_tech)  
+
 
     buzz_feed = pd.read_csv(
         "data/Buzzfeed.csv",
@@ -30,7 +58,7 @@ def main():
             "Symbol":"category"
         })
     
-    (buzz_feed_ds,buzz_feed_schema,buzz_feed_metric) = observations_for_df(df_name="data/Buzzfeed.csv",\
+    (buzz_feed_ds,buzz_feed_schema) = observations_for_df(df_name="data/Buzzfeed.csv",\
                         df_format="csv",\
                         df=buzz_feed)
         
@@ -42,15 +70,49 @@ def main():
 
     monthly_assets.to_csv("data/monthly_assets.csv",index=False)
 
-    (monthly_assets_ds,monthly_assets_schema,monthly_assets_metric)  = observations_for_df(df_name="data/monthly_assets.csv",\
+    (monthly_assets_ds,monthly_assets_schema)  = observations_for_df(df_name="data/monthly_assets.csv",\
                         df_format="csv",\
                         df=monthly_assets)  
+    
         
     lineage = OutputDataLineage(schema=monthly_assets_schema,\
                       input_schemas_mapping=OutputDataLineage.generate_direct_mapping(output_schema=monthly_assets_schema,\
                                                                                       input_schemas=[app_tech_schema,buzz_feed_schema]))
     
     print(json.dumps(lineage))
+
+
+    lineage_exec = DataLineageExecution(
+        lineage=lineage,\
+        application_execution=app_exec
+    )
+
+    print(json.dumps(lineage_exec))
+
+    app_tech_metric = DataMetrics(
+        metrics=DataMetrics.extract_metrics_from_dataframe(df=app_tech),
+        schema=app_tech_schema,\
+        lineage_execution=lineage_exec
+    )
+
+    print(json.dumps(app_tech_metric))
+
+
+    buzz_feed_metric =  DataMetrics(
+        metrics=DataMetrics.extract_metrics_from_dataframe(df=buzz_feed),
+        schema=buzz_feed_schema,\
+        lineage_execution=lineage_exec
+    )
+
+    print(json.dumps(buzz_feed_metric))
+
+    monthly_assets_metric = DataMetrics(
+        metrics=DataMetrics.extract_metrics_from_dataframe(df=monthly_assets),
+        schema=monthly_assets_schema,\
+        lineage_execution=lineage_exec
+    )
+
+    print(json.dumps(monthly_assets_metric))
 
 
 if __name__=="__main__":
--- model.py
+++ model.py
@@ -7,6 +7,7 @@ from typing import List,Tuple
 from functools import reduce
 from math import isnan
 from numbers import Number
+from datetime import datetime
 
 def _default(self, obj):
     return getattr(obj.__class__, "to_json", _default.default)(obj)
@@ -67,6 +68,67 @@ class ApplicationRepository:
             "application":self.application.id
         }
 
+class User:
+    def __init__(self,name:str):
+        self.name = name
+
+        self.id = md5(self.name.encode("utf-8")).hexdigest()
+
+    def to_json(self):
+        return {
+            "id":self.id,
+            "name":self.name
+        }
+    
+    @staticmethod
+    def fetch_git_author()->str:
+
+        repo = git.Repo(os.getcwd(),\
+                        search_parent_directories=True)
+        
+        commit = repo.head.commit
+
+        code_author = commit.author.name
+
+        return code_author
+
+class ApplicationVersion:
+
+    def __init__(self,\
+                 version:str,\
+                 user:User,\
+                 application_repo:ApplicationRepository):
+        
+        self.version = version
+        self.user = user
+        self.application_repo = application_repo
+
+        id_content = ",".join([version,user.id,application_repo.id])
+
+        self.id = md5(id_content.encode("utf-8")).hexdigest()
+
+    def to_json(self):
+        return {
+            "id":self.id,
+            "version":self.version,
+            "user":self.user.id,
+            "application_repository":self.application_repo.id
+        }
+    
+    @staticmethod
+    def fetch_git_version()->str:
+
+        repo = git.Repo(os.getcwd(),\
+                        search_parent_directories=True)
+        
+        commit = repo.head.commit
+
+        code_version = commit.hexsha
+
+        return code_version
+
+
+
 class DataSource:
     def __init__(self,location:str,format:str=None):
         self.location = location
@@ -123,55 +185,28 @@ class Schema:
         ))
 
         return fields
-    
-class DataMetrics:
 
-    def __init__(self,metrics:List[Tuple[str,float]],schema:Schema):
+class ApplicationExecution:
+    def __init__(self,\
+                 application_version:ApplicationVersion,\
+                 user:User):
+        
+        self.application_version = application_version
 
-        self.metrics = metrics
-        self.schema = schema
+        self.user = user
 
-        id_content = ",".join([schema.id])
+        id_content = ",".join([application_version.id,user.id])
 
         self.id = md5(id_content.encode("utf-8")).hexdigest()
-    
-    def to_json(self):
-        fields = reduce(
-            lambda x,y:dict(**x,**y),
-            map(
-                lambda f: {f[0]:f[1]}
-            ,self.metrics)
-        )
 
+    def to_json(self):
         return {
             "id":self.id,
-            "metrics":fields,
-            "schema":self.schema.id
+            "application_version":self.application_version.id,
+            "user":self.user.id
         }
-    
-    @staticmethod
-    def extract_metrics_from_dataframe(df:pd.DataFrame)->List[Tuple[str,float]]:
-        
-        describe_value = df.describe(include="all")
-        
-        metrics = {}
 
-        empty_nan_filter = lambda x: isinstance(x[1],Number) and not isnan(x[1])
 
-        for field in describe_value.columns[1:]:
-
-            field_dict = describe_value[field].to_dict()
-
-            mapped_items = map(lambda x: (field + "." + x[0], x[1]), field_dict.items())
-
-            filtered_items = filter(empty_nan_filter, mapped_items)
-
-            metric_description = dict(filtered_items)
-
-            metrics.update(metric_description)
-
-        return list(metrics.items())
-    
 class OutputDataLineage:
     def __init__(self,schema:Schema,input_schemas_mapping:List[Tuple[Schema,dict]]):
         self.schema = schema
@@ -224,24 +259,100 @@ class OutputDataLineage:
         linearized = linearized[:-1]
 
         return linearized
+    
+class DataLineageExecution:
+    def __init__(self,\
+                 lineage:OutputDataLineage,\
+                 application_execution:ApplicationExecution):
+        
+        self.lineage = lineage
+        self.application_execution = application_execution
 
-def observations_for_df(df_name: str, df_format: str, df: pd.DataFrame,is_print_observation=True)->Tuple[DataSource,Schema,DataMetrics]:
+        self.start_time = datetime.now().isoformat()
+
+        id_content = ",".join([self.lineage.id,\
+                               self.application_execution.id,\
+                               self.start_time])
+
+        self.id = md5(id_content.encode("utf-8")).hexdigest()
+
+    def to_json(self):
+        return {
+            "id":self.id,
+            "lineage":self.lineage.id,
+            "application_execution":self.application_execution.id,
+            "start_time":self.start_time
+        }
+
+class DataMetrics:
+
+    def __init__(self,\
+                 metrics:List[Tuple[str,float]],\
+                 schema:Schema,\
+                 lineage_execution:DataLineageExecution):
+
+        self.metrics = metrics
+
+        self.schema = schema
+
+        self.lineage_execution = lineage_execution
+
+        id_content = ",".join([schema.id,lineage_execution.id])
+
+        self.id = md5(id_content.encode("utf-8")).hexdigest()
+    
+    def to_json(self):
+        fields = reduce(
+            lambda x,y:dict(**x,**y),
+            map(
+                lambda f: {f[0]:f[1]}
+            ,self.metrics)
+        )
+
+        return {
+            "id":self.id,
+            "metrics":fields,
+            "schema":self.schema.id,
+            "lineage_execution":self.lineage_execution.id
+        }
+    
+    @staticmethod
+    def extract_metrics_from_dataframe(df:pd.DataFrame)->List[Tuple[str,float]]:
+        
+        describe_value = df.describe(include="all")
+        
+        metrics = {}
+
+        empty_nan_filter = lambda x: isinstance(x[1],Number) and not isnan(x[1])
+
+        for field in describe_value.columns[1:]:
+
+            field_dict = describe_value[field].to_dict()
+
+            mapped_items = map(lambda x: (field + "." + x[0], x[1]), field_dict.items())
+
+            filtered_items = filter(empty_nan_filter, mapped_items)
+
+            metric_description = dict(filtered_items)
+
+            metrics.update(metric_description)
+
+        return list(metrics.items())
+        
+
+def observations_for_df(df_name: str,\
+                        df_format: str,\
+                        df: pd.DataFrame,\
+                        is_print_observation=True)->Tuple[DataSource,Schema]:
 
     data_source = DataSource(location=df_name,format=df_format)
 
     schema = Schema(fields=Schema.extract_fields_from_dataframe(df=df),\
                     data_source=data_source)
     
-    metric = DataMetrics(metrics=DataMetrics.extract_metrics_from_dataframe(df=df),\
-                         schema=schema)
-    
     if is_print_observation:
         print(json.dumps(data_source))
         print(json.dumps(schema))
-        print(json.dumps(metric))
 
 
-
-
-
-    return (data_source,schema,metric)
+    return (data_source,schema)
