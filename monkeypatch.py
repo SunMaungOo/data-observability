e2847aabbb3e0bb1549319c4250aa329ea1a4038
Sun Maung Oo <2162432+SunMaungOo@users.noreply.github.com>
2025-06-04 12:55:04 +0630

    add monkey patch code for data source and schema

--- monkeypatch.py
+++ monkeypatch.py
@@ -1,11 +1,23 @@
 import pandas as pd
+from model import DataSource,Schema
 
 orginal_read_csv = pd.read_csv
 
 def read_csv_with_data_observability(*args,**kawrgs)->pd.DataFrame:
     df = orginal_read_csv(*args,**kawrgs)
-    print("My monkey patch code")
+
+    file_name = args[0][0:args[0].rfind(".")]
+
+    file_format = args[0][args[0].rfind('.') + 1:]
+
+    data_source = DataSource(location=file_name,\
+                             format=file_format)
+
+    schema = Schema(Schema.extract_fields_from_dataframe(df=df),\
+                    data_source=data_source)
+
     return df
+
 
 
 # notice that we have dynamically inject our read_csv_with_data_observability function as pd.read_csv
